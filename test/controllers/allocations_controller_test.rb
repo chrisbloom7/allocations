993e40205e81a0d4fadebc0a6caaa0366c0bee24
Chris Bloom <chrisbloom7@github.com>
2025-03-04 04:28:37 -0500

    add tests

---
+++ test/controllers/allocations_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class AllocationsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should show allocation" do
+    post allocation_url, params: {
+      allocation_amount: 100,
+      investor_amounts: [
+        { name: 'Investor A', requested_amount: 150, average_amount: 100 },
+        { name: 'Investor B', requested_amount: 50, average_amount: 25 }
+      ]
+    }
+    assert_response :success
+    json_response = JSON.parse(response.body)
+    assert_equal 100, json_response['allocation_amount']
+    assert_equal 100, json_response['allocated']
+    assert_equal 0, json_response['remaining']
+    assert_equal 'fully_funded', json_response['funding_status']
+  end
+end
--- test/services/allocation_service_test.rb
+++ test/services/allocation_service_test.rb
@@ -2,61 +2,69 @@ require 'test_helper'
 
 class AllocationServiceTest < ActiveSupport::TestCase
   def setup
-    @allocation_data = {
-      allocation_amount: 100,
-      investor_amounts: [
-        { name: 'Investor A', requested_amount: 150, average_amount: 100 },
-        { name: 'Investor B', requested_amount: 50, average_amount: 25 }
-      ]
-    }
-    @allocation = AllocationService.new(@allocation_data)
+    @allocation_amount = 100
+    @investor_amounts = [
+      { name: 'Investor A', requested_amount: 150, average_amount: 100 },
+      { name: 'Investor B', requested_amount: 50, average_amount: 25 }
+    ]
+    @service = AllocationService.new(allocation_amount: @allocation_amount, investor_amounts: @investor_amounts)
   end
 
-  test "initialization" do
-    assert_equal 100.0, @allocation.allocation
-    assert_equal 2, @allocation.investors.size
+  test "should allocate funds correctly" do
+    @service.fund!
+    assert_equal 100, @service.allocated
+    assert_equal 0, @service.remaining
+    assert_equal :fully_funded, @service.funding_status
   end
 
-  test "fund!" do
-    @allocation.fund!
-    assert_equal 100.0, @allocation.allocated
-    assert_equal :fully_funded, @allocation.funding_status
-
-    expected_investments = { 'Investor A' => 80.0, 'Investor B' => 20.0 }
-    assert_equal expected_investments, @allocation.to_h
+  test "should reset allocation" do
+    @service.fund!
+    @service.reset!
+    assert_equal 0, @service.allocated
+    assert_equal :unfunded, @service.funding_status
   end
 
-  test "funding status" do
-    assert_equal :unfunded, @allocation.funding_status
-    @allocation.fund!
-    assert_equal :fully_funded, @allocation.funding_status
+  test "should return correct funding status" do
+    assert_equal :unfunded, @service.funding_status
+    @service.fund!
+    assert_equal :fully_funded, @service.funding_status
+  end
+
+  test "should map investor amounts correctly" do
+    investors = @service.send(:map_investor_amounts, @investor_amounts)
+    assert_equal 2, investors.size
+    assert_equal 'Investor A', investors.first.name
+    assert_equal 150, investors.first.requested_amount
+    assert_equal 100, investors.first.average_amount
   end
 end
 
-class AllocationServiceInvestorTest < ActiveSupport::TestCase
+class InvestorTest < ActiveSupport::TestCase
   def setup
     @investor_data = { name: 'Investor A', requested_amount: 150, average_amount: 100 }
     @investor = AllocationService::Investor.new(@investor_data)
   end
 
-  test "initialization" do
+  test "should initialize investor correctly" do
     assert_equal 'Investor A', @investor.name
-    assert_equal 150.0, @investor.requested_amount
-    assert_equal 100.0, @investor.average_amount
+    assert_equal 150, @investor.requested_amount
+    assert_equal 100, @investor.average_amount
     assert_equal 0, @investor.invested
   end
 
-  test "fund" do
+  test "should fund investor correctly" do
     amount = @investor.fund(80)
-    assert_equal 80.0, amount
-    assert_equal 80.0, @investor.invested
-
-    amount = @investor.fund(100)
-    assert_equal 70.0, amount
-    assert_equal 150.0, @investor.invested
+    assert_equal 80, amount
+    assert_equal 80, @investor.invested
   end
 
-  test "investment status" do
+  test "should not overfund investor" do
+    amount = @investor.fund(160)
+    assert_equal 150, amount
+    assert_equal 150, @investor.invested
+  end
+
+  test "should return correct investment status" do
     assert_equal :uninvested, @investor.investment_status
     @investor.fund(80)
     assert_equal :partially_invested, @investor.investment_status
@@ -64,9 +72,10 @@ class AllocationServiceInvestorTest < ActiveSupport::TestCase
     assert_equal :fully_invested, @investor.investment_status
   end
 
-  test "available?" do
-    assert @investor.available?
-    @investor.fund(150)
-    refute @investor.available?
+  test "should reset investor correctly" do
+    @investor.fund(80)
+    @investor.reset!
+    assert_equal 0, @investor.invested
+    assert_equal :uninvested, @investor.investment_status
   end
 end
